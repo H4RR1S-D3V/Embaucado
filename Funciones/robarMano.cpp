925b877294f16bb038605f3df57542e84a62e193
H4RR1S-D3V <harrisdm.dev@gmail.com>
2024-06-15 16:41:21 -0300

    robarMano()
    Creación de función que llena los vectores de ambos jugadores

---
+++ Funciones/robarMano.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "../Funciones/robarMano.h"
+
+using namespace std;
+
+/// ESTA FUNCIÓN GENERA N NUMEROS "ALEATOREOS" Y LOS GUARDA EN UN VECTOR (MANO DEL JUGADOR)
+
+void robarMano(int v[], int tam)
+{
+    for(int y=0; y<tam; y++)
+    {
+        v[y] = rand() % 20+1;   // CARTA CODIFICADA DEL 1 AL 20
+    }
+}
---
+++ Funciones/robarMano.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void robarMano(int v[], int tam);
