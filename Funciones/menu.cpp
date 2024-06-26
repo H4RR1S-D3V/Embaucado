b9be30cea182f12b39c3d46132821543ece8e4f0
H4RR1S-D3V <harrisdm.dev@gmail.com>
2024-06-26 13:45:36 -0300

    actualizacion menu
    modularización del codigo

--- Funciones/menu.cpp
+++ Funciones/menu.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <ctime>
 #include "rlutil.h"
-//#include "mostrarEstadisticas.h"
+#include "mostrarEstadisticas.h"
 #include "jugar.h"
-
-
+#include "mostrarCreditos.h"
+#include "mostrarReglas.h"
 
 using namespace std;
 
 
-void menu()
+void menu(int vTopTresPuntos[], string vTopTresNombres[])
 {
+    /// VECTORES ESTADISTICAS
+
     int opciones = 1;
     int posicionY = 0;
 
@@ -127,63 +129,22 @@ void menu()
             case 0:
                 ///EN CONSTRUCCION-FALTA ARMAR LA FUNCION PARA JUGAR
                 rlutil::cls();
-                /// FUNICON DE JUEGO
-                jugar("guada", "manu");
-                ///ACA PONER FUNCION PARA INGRESAR NOMBRES-POR AHORA HAY ESTO
-                rlutil::cls();
+                /// FUNCION DE JUEGO
+                jugar(vTopTresPuntos, vTopTresNombres);
                 break;
             case 1:
                 ///REGLAS
-                rlutil::cls();
-
-                rlutil::locate(30,15);
-
-                cout << (char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<  (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << endl;
-
-                rlutil::locate(30,35);
-
-                cout << (char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<(char) 205 <<  (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << (char) 205 << endl;
-
-                rlutil::locate(75 ,17);
-                cout << (char) 168 <<"DE QUE SE TRATA?"<< endl;
-
-                rlutil::locate(40,18);
-                cout << "Es un juego de naipes para dos a cuatro personas,basado en las cartas de la baraja francesa." << endl;
-                rlutil::locate(41,20);
-                cout << "Las cartas de la baraja francesa a utilizar son : 10, J, Q, K y A de las cuatro figuras:"<< endl;
-                rlutil::locate(60,22);
-                cout << " Corazon" << " "<< (char) 03  << ", Diamante" << " " <<(char) 04  << ", Picas" << " "<<  (char) 06 << " " << " y Trebol " << (char) 05 << "." << endl;
-                rlutil::locate(38,23);
-                cout << "Existen 4 cartas sin valor que ocupan solo una de las cuatro figuras. Estas son las cartas embaucadoras." << endl;
-
-                rlutil::locate(77,26);
-                cout << "MODO DE JUEGO"<< endl;
-                rlutil::locate(40,27);
-                cout << "A lo largo de 3 rondas los jugadores reciben cinco cartas al azar que se denominan mano."<< endl;
-                rlutil::locate(50,29);
-                cout << "Ademas, se da vuelta sobre la mesa una carta de las embaucadoras." << endl;
-                rlutil::locate(32,30);
-                cout << "Se suman los valores de las cartas que salen obviando las cartas cuyas figuras coincidan con la embaucadora."<< endl;
-                rlutil::locate(37,33);
-                cout << "Una partida de embaucadora la gana quien haya obtenido más puntos a lo largo de las tres rondas."<< endl;
-                rlutil::anykey();
-                rlutil::cls();
+                mostrarReglas();
                 break;
             case 2:
                 ///NO MUESTRA EL COUT-REVISARLO
                 rlutil::cls();
-                /// mostrarEstadisticas();
+                mostrarEstadisticas(vTopTresPuntos, vTopTresNombres);
                 rlutil::anykey();
                 rlutil::cls();
                 break;
             case 3:
-                rlutil::cls();
-                rlutil::locate(2, 5);
-                cout << "CREDITOS"<< endl;
-                cout << "GRUPO 1 :"<< endl;
-                cout << "FRANCHI LEIVA GUADALUPE - LEGAJO: 30971"<< endl;
-                rlutil::anykey();
-                rlutil::cls();
+                mostrarCreditos();
                 break;
             case 4:
                 opciones = 0;
--- Funciones/menu.h
+++ Funciones/menu.h
@@ -1,3 +1,3 @@
 #pragma once
 
-void menu();
+void menu(int vTopTresPuntos[], std::string vTopTresNombres[]);
